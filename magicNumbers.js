fb6ce93fa05ae309a9d72344b3a4cca2683de89c
Clayton Lin <clin.acc.dev@gmail.com>
2018-10-04 11:47:14 -0500

    Init magicNumbers

---
+++ magicNumbers.js
@@ -0,0 +1,23 @@
+// Print all the "magic numbers" between 100 and 1000.
+
+// A magic number is defined as an integer which
+// 1) Has no repeating digits
+// 2) follows the "magic pattern"
+
+// The magic pattern goes as follows:
+//   Start with the leftmost digit N, and from that position move N units to the right, 
+//   rolling over as needed. Repeat the process with the new digit you are looking at. 
+//   Continue this process. A magic number will encounter each digit only once to end 
+//   on the leftmost digit.
+
+// Example Magic Number
+// 1623
+// starting with 1, move 1 digit to the right to hit 6.
+// starting at 6, move 6 digits to the right (wrapping around once) to hit 3
+// starting at 3, move 3 digits to the right (wrapping around once) to hit 2
+// starting at 2, move 2 digits to the right (wrapping around once) to finish on 1
+
+
+
+
+
