cef576f486eb518e0247244685e16c96f5b12219
Clayton Lin <clin.acc.dev@gmail.com>
2018-10-08 14:30:49 -0500

    Make progress on trappingRainWater

--- unsolved-problems/trappingRainWater/index.js
+++ unsolved-problems/trappingRainWater/index.js
@@ -7,37 +7,101 @@
 // o: 6
 
 /**
- * @param {number[]} height
+ * @param {number[]} heights
  * @return {number}
  */
 
-const trap = (height) => {
-  let i = 0;
-  let water = 0;
-  let curHeight = 0;
+// const trap = (heights) => {
+//   let i = 0;
+//   let water = 0;
+//   let curHeight = 0;
   
-  while (i < arrray.length) {
-      curHeight = height[i];
-      if (height[i + 1] < height[i]) {
-          const tuple = processValley(i, height);
-          i += tuple[0];
-          water += tuple[1];
+//   while (i < heights.length) {
+//       curHeight = heights[i];
+//       if (heights[i + 1] < heights[i]) {
+//           const tuple = processValley(i, heights);
+//           console.log(tuple);
+//           i = tuple[0];
+//           water += tuple[1];
+//       } else {
+//           i++;
+//       }
+//   }
+  
+//   return water;
+// };
+
+// const processValley = (i, array) => {
+//   let water = 0;
+//   let peakHeight = array[i];
+  
+//   while (array[i] <= peakHeight) {
+//       water += peakHeight - array[i];
+//       console.log(i, peakHeight, array[i], water)
+//       i++;
+//   }
+  
+//   return [i, water];
+// }
+
+const trap = (heights) => {
+  // horizontal approach
+  let water = 0;
+  let highest = 0;
+  let lowest = 0;
+  const elevations = {};
+
+  heights.forEach((height, i) => {
+    console.log('------------------------------------')
+    console.log('index ', i)
+    console.log('elevations before: ', elevations);
+
+    // if (height > 0) {
+      if (height > highest) {
+        console.log('elevations will clear');
+        delete(elevations[0]);
+        for (let h in elevations) {
+          if (h > lowest) {
+            water += i - elevations[h];
+          }
+          delete(elevations[h]);
+        }
+        elevations[height] = i + 1;
+        highest = height;
+
+      } else if (elevations[height]) {
+        console.log('add water');
+        delete(elevations[0]);
+        for (let h in elevations) {
+          if (h <= height) {
+            if (h > lowest) {
+              water += i - elevations[height];
+            }
+            delete(elevations[h]);
+          }
+        }
+
       } else {
-          i++;
+        console.log('add elevation');
+        lowest = height;
+        const drop = heights[i - 1] - height;
+        console.log('drop', drop)
+        elevations[height] = i + 1;
+        for (let j = 1; j <= drop; j++) {
+          elevations[height + j] = i;
+        }
       }
-  }
+    // } 
+
+    console.log('elevations after: ', elevations);
+    console.log(`height: ${height}, highest: ${highest}, water: ${water}`);
+  });
   
   return water;
 };
 
-const processValley = (i, array) => {
-  let water = 0;
-  let peakHeight = array[i];
-  
-  while (array[i] <= peakHeight) {
-      water += peakHeight - array[i];
-      i++;
-  }
-  
-  return [i, water];
-}
+
+
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])); // 6
+console.log(trap([2,0,2])); // 2
+console.log(trap([2,1,0,2]); // 3
