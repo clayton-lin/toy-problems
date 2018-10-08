7490a8d21bb5f1a872bce8a45fb7639977de7eac
Clayton Lin <clin.acc.dev@gmail.com>
2018-10-08 11:53:51 -0500

    Init intersectionOfTwoArrays

---
+++ intersectionOfTwoArrays.js
@@ -0,0 +1,16 @@
+// Given two arrays, write a function to compute their intersection
+
+// i: arr1 = [1,3,3,1], arr2 = [3,3]
+// o: [3,3]
+
+// i: arr1 = [3,8,5], arr2 = [8,3,8,7,4]
+// o: [4,9]
+
+// each element in the result should appear as many times as it shows in both arrays
+// the reasult can be in any order
+
+// what if the array is already sorted? how would you optimize?
+// what if arr1 size is small compared to arr2? which algo is better?
+// what if elements of arr2 are stored on disk and memory is limited
+// such that you cannot load all elements into memory at once?
+
