952c363eef01d0a8483e0c14b717ba3fcfa6e1d2
Clayton Lin <clin.acc.dev@gmail.com>
2018-11-13 00:06:18 -0600

    Complete tests for sameFrequency

--- solved-problems/sameFrequency/test.js
+++ solved-problems/sameFrequency/test.js
@@ -4,6 +4,18 @@ test('sameFrequency function should exist', () => {
   expect(sameFrequency).toBeDefined();
 });
 
-test('', () => {
-  expect(___).toEqual();
+test('should return true if numbers have the same frequency of numbers', () => {
+  expect(sameFrequency(182, 281)).toEqual(true);
+});
+
+test('should return false if the digits are not the same', () => {
+  expect(sameFrequency(34, 14)).toEqual(false);
+});
+
+test('should return true if numbers have the same frequency of numbers for larger numbers', () => {
+  expect(sameFrequency(3589578, 5879385)).toEqual(true);
+});
+
+test('should return false if numbers are different number of digits', () => {
+  expect(sameFrequency(22, 222)).toEqual(false);
 });
