2a37390cf54dedcdbd544b2080407fa51d5a62b2
Clayton Lin <clin.acc.dev@gmail.com>
2018-11-15 19:11:31 -0600

    Init maxSubarraySum

---
+++ unsolved-problems/maxSubarraySum/index.js
@@ -0,0 +1,13 @@
+// Given an array of integers and a number, write a function that finds the
+// maximum sum if a subarray with the passed number
+// Return null if there is no possible subarray
+
+const masSubarraySum = (arr, size) => {
+
+
+  // time complexity: O()
+  // space complexity: O()
+  return null;
+};
+
+module.exports = masSubarraySum;
---
+++ unsolved-problems/maxSubarraySum/test.js
@@ -0,0 +1,9 @@
+const msxSubarraySum = require('./index');
+
+test('msxSubarraySum function should exist', () => {
+  expect(msxSubarraySum).toBeDefined();
+});
+
+test('', () => {
+  expect(msxSubarraySum()).toEqual();
+});
