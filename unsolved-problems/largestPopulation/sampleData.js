a067352e0d3fc50ed29c74c8b12be2d891ea2058
Clayton Lin <clin.acc.dev@gmail.com>
2018-11-20 23:25:59 -0600

    Update largestPopulation sample data

---
+++ unsolved-problems/largestPopulation/sampleData.js
@@ -0,0 +1,52 @@
+module.exports = [
+  {
+    name: 'Eddard',
+    birthYear: 1987,
+    deathYear: 2018
+  },
+  {
+    name: 'Sansa',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Arya',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Rob',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Jon',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Bran',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Rickon',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Catelyn',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Benjen',
+    birthYear: ,
+    deathYear: 
+  },
+  {
+    name: 'Lyanna',
+    birthYear: ,
+    deathYear: 
+  },
+];
--- unsolved-problems/largestPopulation/test.js
+++ unsolved-problems/largestPopulation/test.js
@@ -1,9 +1,10 @@
 const largestPopulation = require('./index');
+const starks = require('./sampleData')
 
 test('largestPopulation function should exist', () => {
   expect(largestPopulation).toBeDefined();
 });
 
-test('', () => {
-  expect(largestPopulation()).toEqual();
+test('Should return correct year', () => {
+  expect(largestPopulation(starks)).toEqual();
 });
