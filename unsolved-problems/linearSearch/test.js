ef187cfdebc06a6a06e5f827043c9e6e890f597d
Clayton Lin <clin.acc.dev@gmail.com>
2018-11-20 23:12:37 -0600

    Init linearSearch

---
+++ unsolved-problems/linearSearch/test.js
@@ -0,0 +1,9 @@
+const linearSearch = require('./index');
+
+test('linearSearch function should exist', () => {
+  expect(linearSearch).toBeDefined();
+});
+
+test('', () => {
+  expect(linearSearch()).toEqual();
+});
